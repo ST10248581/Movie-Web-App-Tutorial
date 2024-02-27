67c196577c153f3a86ea50a94d476e8f01d3c995
Troy Krause <troykrause07@gmail.com>
2024-02-27 20:23:38 +0200

    First Commit

---
+++ Data/CloudDevWebAppContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CloudDevWebApp.Models;
+
+namespace CloudDevWebApp.Data
+{
+    public class CloudDevWebAppContext : DbContext
+    {
+        public CloudDevWebAppContext (DbContextOptions<CloudDevWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies> Movies { get; set; } = default!;
+
+        public DbSet<CloudDevWebApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CloudDevWebApp.Data;
+using CloudDevWebApp.Models;
+
+namespace CloudDevWebApp.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly CloudDevWebApp.Data.CloudDevWebAppContext _context;
+
+        public DetailsModel(CloudDevWebApp.Data.CloudDevWebAppContext context)
+        {
+            _context = context;
+        }
+
+      public Movie Movie { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Movie = movie;
+            }
+            return Page();
+        }
+    }
+}
